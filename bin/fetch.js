50e615a89e261ce766093f7a4ab5b3ab21c3ae56
Willin Wang <willin@willin.org>
2021-07-05 15:18:05 +0800

    fix: :bug: fix lint

--- bin/fetch.js
+++ bin/fetch.js
@@ -1,10 +1,10 @@
 const path = require('path');
 const { writeFileSync } = require('fs');
-const { execSync } = require('child_process');
+// const { execSync } = require('child_process');
 const { github } = require('./lib');
 
 (async () => {
   const ghResult = await github().then(data => JSON.stringify(data, null, 2));
   writeFileSync(path.resolve(__dirname, '../assets/js/github.js'), `export default ${ghResult}`, 'utf-8');
-  execSync('npm run lint');
+  // execSync('npm run lint');
 })();
